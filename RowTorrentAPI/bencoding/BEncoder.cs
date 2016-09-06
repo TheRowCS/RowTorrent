573180d44d6dba1517101fafc68468b076c6f8f0
Bendat <benaherne@outlook.ie>
2016-09-06 17:51:21 +0100

    Added skeletal bencoder class

--- RowTorrentAPI/bencoding/BEncoder.cs
+++ RowTorrentAPI/bencoding/BEncoder.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace RowTorrentAPI.bencoding {
-    class BEncoder {
+    public static class BEncoder {
+        public static void Bencode(Object o, StreamWriter sw) {       
+            if ((Type)o == typeof(Dictionary<string, BElement>)) {
+                
+            }
+            else if ((Type)o == typeof(List<BElement>)) {
+
+            }
+            else {
+                switch (Type.GetTypeCode(o.GetType())) {
+                    case TypeCode.String:
+                        break;
+                    case TypeCode.Int64:
+                        break;
+                    case TypeCode.Int32:
+                        break;
+                    case TypeCode.Int16:
+                        break;
+                    default:
+                        throw new ArgumentException(o.GetType() + "Cannot be bencoded.");
+                }
+            }
+        }
     }
 }
